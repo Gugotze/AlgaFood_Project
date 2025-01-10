3987fe35c3003a1a0d779f6160d5dc0e5372328f
Gustavo Fonseca Pitombeira <gustavofonsecap@hotmail.com>
2025-01-10 08:23:22 -0300

    Modulo 3 feat: Retirando projeto antigo

--- src/main/java/com/algaworks/algafood/notificacao/NotificadorClient.java
+++
@@ -1,7 +0,0 @@
-package com.algaworks.algafood.notificacao;
-
-import com.algaworks.algafood.model.Cliente;
-
-public interface NotificadorClient {
-    void notificar(Cliente cliente, String s);
-}

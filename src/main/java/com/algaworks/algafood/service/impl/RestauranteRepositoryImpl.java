343d9c106c48feebb5c79f016d67a8ccc84ed6c3
Gustavo Fonseca Pitombeira <gustavofonsecap@hotmail.com>
2025-01-23 09:29:07 -0300

    Modulo-5 feat: 05.13 - Criando primeira consulta com Criteria

--- src/main/java/com/algaworks/algafood/service/impl/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/service/impl/RestauranteRepositoryImpl.java
@@ -5,11 +5,10 @@ import com.algaworks.algafood.repository.RestauranteRepositoryQueries;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaQuery;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.List;
 
 @Repository
@@ -20,31 +19,12 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 
     @Override
     public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
-        var jpql = new StringBuilder();
-        jpql.append("from Restaurante where 0 = 0 ");
 
-        var parametros = new HashMap<String, Object>();
+        CriteriaQuery<Restaurante> criteria = manager.getCriteriaBuilder().createQuery(Restaurante.class);
+        criteria.from(Restaurante.class);
 
-        if (StringUtils.hasLength(nome)) {
-            jpql.append("and nome like :nome ");
-            parametros.put("nome", "%" + nome + "%");
-        }
-
-        if (taxaFreteInicial != null) {
-            jpql.append("and taxaFrete >= :taxaFreteInicial ");
-            parametros.put("taxaFreteInicial", taxaFreteInicial);
-        }
-
-        if (taxaFreteFinal != null) {
-            jpql.append("and taxaFrete <= :taxaFreteFinal ");
-            parametros.put("taxaFreteFinal", taxaFreteFinal);
-        }
-
-        TypedQuery <Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
-
-        parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
-
+        TypedQuery<Restaurante> query = manager.createQuery(criteria);
         return query.getResultList();
-    }
 
+    }
 }

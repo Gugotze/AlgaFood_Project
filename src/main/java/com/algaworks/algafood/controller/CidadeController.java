a8bd57297677070ce05ca9947ca4a4081ec7dd96
Gustavo Fonseca Pitombeira <gustavofonsecap@hotmail.com>
2025-02-06 12:53:40 -0300

    Modulo 8 feat: 08.12 - Tratando exceções com ExceptionHandler

--- src/main/java/com/algaworks/algafood/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/controller/CidadeController.java
@@ -4,13 +4,16 @@ import com.algaworks.algafood.domain.Cidade;
 import com.algaworks.algafood.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.exception.EstadoNaoEncontradoException;
 import com.algaworks.algafood.exception.NegocioException;
+import com.algaworks.algafood.exceptionHandler.Problema;
 import com.algaworks.algafood.repository.CidadeRepository;
 import com.algaworks.algafood.service.CadastroCidadeService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -53,9 +56,9 @@ public class CidadeController {
 
         BeanUtils.copyProperties(cidade, cidadeAtual, "id");
 
-        try{
+        try {
             return cadastroCidade.salvar(cidadeAtual);
-        }catch (EstadoNaoEncontradoException e){
+        } catch (EstadoNaoEncontradoException e) {
             throw new NegocioException(e.getMessage());
         }
     }
@@ -67,4 +70,30 @@ public class CidadeController {
         cadastroCidade.excluir(cidadeId);
 
     }
+
+    @ExceptionHandler(EntidadeNaoEncontradaException.class)
+    public ResponseEntity<?> tratarEstadoNaoEncontradoException(EntidadeNaoEncontradaException e) {
+
+        Problema problema = Problema.builder()
+                .dataHora(LocalDateTime.now())
+                .mensagem(e.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(problema);
+    }
+
+    @ExceptionHandler(NegocioException.class)
+    public ResponseEntity<?> tratarNegocioException(NegocioException e) {
+
+        Problema problema = Problema.builder()
+                .dataHora(LocalDateTime.now())
+                .mensagem(e.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(problema);
+    }
+
+
 }

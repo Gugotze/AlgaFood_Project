e4812a803444e6ffda5e43f3a05f9d7d4540c1de
Gustavo Fonseca Pitombeira <gustavofonsecap@hotmail.com>
2025-02-24 09:46:38 -0300

    Modulo-11 feat: 11.03 - Criando classes de mixin para usar as anotações do Jackson

--- src/main/java/com/algaworks/algafood/domain/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/Restaurante.java
@@ -1,13 +1,12 @@
 package com.algaworks.algafood.domain;
 
 import com.algaworks.algafood.core.validation.Groups;
-import com.algaworks.algafood.core.validation.Multiplo;
 import com.algaworks.algafood.core.validation.ValorZeroIncluiDescricao;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.groups.ConvertGroup;
 import jakarta.validation.groups.Default;
 import lombok.Data;
@@ -28,51 +27,44 @@ import java.util.List;
         descricaoObrigatoria = "Frete Grátis")
 public class Restaurante {
 
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @EqualsAndHashCode.Include
     private Long id;
 
     @NotBlank
     @Column(nullable = false)
     private String nome;
 
-    @Column(name = "taxa_frete", nullable = false)
     @NotNull
     @PositiveOrZero
-    @Multiplo(numero = 5)
+    @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
 
-    @ManyToOne
-    @JoinColumn(name = "cozinha_id", nullable = false)
-    @NotNull
     @Valid
     @ConvertGroup(from = Default.class, to = Groups.CozinhaId.class)
-    @JsonIgnoreProperties(value = "nome" , allowGetters = true)
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
 
     @Embedded
-    @JsonIgnore
     private Endereco endereco;
 
-    @Column(nullable = false, columnDefinition = "timestamp")
     @CreationTimestamp
-    @JsonIgnore
+    @Column(nullable = false, columnDefinition = "timestamp")
     private LocalDateTime dataCadastro;
 
-    @Column(nullable = false, columnDefinition = "timestamp")
     @UpdateTimestamp
-    @JsonIgnore
+    @Column(nullable = false, columnDefinition = "timestamp")
     private LocalDateTime dataAtualizacao;
 
-    @ManyToMany//(fetch = FetchType.EAGER)
-   // @JsonIgnore
+    @ManyToMany
     @JoinTable(name = "restaurante_forma_pagamento",
-            joinColumns = @JoinColumn(name = "restaurante_id"),// Defini a coluna que faz referencia a tabela atual
+            joinColumns = @JoinColumn(name = "restaurante_id"),
             inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
     private List<FormaPagamento> formasPagamento = new ArrayList<>();
 
-    @JsonIgnore
     @OneToMany(mappedBy = "restaurante")
     private List<Produto> produtos = new ArrayList<>();
 }

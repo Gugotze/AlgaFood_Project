1fb8554210429d60d7d98f2ff6d6855e4cc51147
Gustavo Fonseca Pitombeira <gustavofonsecap@hotmail.com>
2025-02-27 10:18:49 -0300

    Modulo-11 feat: 11.14 - Adicionando e usando o ModelMapper

--- src/main/java/com/algaworks/algafood/assembler/RestauranteModelAssembler.java
+++ src/main/java/com/algaworks/algafood/assembler/RestauranteModelAssembler.java
@@ -3,23 +3,21 @@ package com.algaworks.algafood.assembler;
 import com.algaworks.algafood.domain.Restaurante;
 import com.algaworks.algafood.dto.CozinhaDTO;
 import com.algaworks.algafood.dto.RestauranteDTO;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
+@AllArgsConstructor
 public class RestauranteModelAssembler {
 
+    private final ModelMapper modelMapper;
+
     public  RestauranteDTO toModel(Restaurante restaurante) {
-        RestauranteDTO restauranteModel = new RestauranteDTO();
-        restauranteModel.setId(restaurante.getId());
-        restauranteModel.setNome(restaurante.getNome());
-        restauranteModel.setTaxaFrete(restaurante.getTaxaFrete());
-        restauranteModel.setCozinha(new CozinhaDTO());
-        restauranteModel.getCozinha().setId(restaurante.getCozinha().getId());
-        restauranteModel.getCozinha().setNome(restaurante.getCozinha().getNome());
-        return restauranteModel;
+        return modelMapper.map(restaurante, RestauranteDTO.class);
     }
 
     public List<RestauranteDTO> toCollectionModel(List<Restaurante> restaurantes) {
--- src/main/java/com/algaworks/algafood/assembler/RestauranteModelDisassembler.java
+++ src/main/java/com/algaworks/algafood/assembler/RestauranteModelDisassembler.java
@@ -1,22 +1,18 @@
 package com.algaworks.algafood.assembler;
 
-import com.algaworks.algafood.domain.Cozinha;
 import com.algaworks.algafood.domain.Restaurante;
 import com.algaworks.algafood.input.RestauranteInput;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class RestauranteModelDisassembler {
 
+    private final ModelMapper modelMapper;
+
     public Restaurante toDomainObject(RestauranteInput restauranteInput) {
-        Restaurante restaurante = new Restaurante();
-        restaurante.setNome(restauranteInput.getNome());
-        restaurante.setTaxaFrete(restauranteInput.getTaxaFrete());
-
-        Cozinha cozinha = new Cozinha();
-        cozinha.setId(restauranteInput.getCozinha().getId());
-
-        restaurante.setCozinha(cozinha);
-        return restaurante;
+        return modelMapper.map(restauranteInput, Restaurante.class);
     }
 }
